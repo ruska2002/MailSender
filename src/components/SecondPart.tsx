28b9513825736e76619e83edaa72eb7ba51be22c
ruska metskhovrishvili <ruso.mecxovrishvili1@gmail.com>
2025-05-20 18:40:54 +0400

    add swiper

--- src/components/SecondPart.tsx
+++ src/components/SecondPart.tsx
@@ -1,41 +1,37 @@
-import star from "/pictures/Frame 416.png";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-export default function SecondPart() {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 4000, min: 1025 },
-      items: 3,
-      partialVisibilityGutter: 0,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 481 },
-      items: 2,
-      partialVisibilityGutter: 60,
-    },
-    mobile: {
-      breakpoint: { max: 480, min: 0 },
-      items: 1,
-      partialVisibilityGutter: 80,
-    },
-  };
+import star from "/pictures/Frame 416.png";
+
+export default function SecondBar() {
   return (
-    <div className="bg-[black] top-[200px]">
-      <Carousel responsive={responsive} partialVisible={true}>
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={3}
+      breakpoints={{
+        0: { slidesPerView: 1.2 },
+        480: { slidesPerView: 2.2 },
+        1024: { slidesPerView: 3 },
+      }}
+    >
+      <SwiperSlide>
         <div className="bg-[#F1F1F1] p-4 text-center">
           <img src={star} className="mx-auto mb-2" />
           <p>უფასო მასტერკლასები</p>
         </div>
+      </SwiperSlide>
+      <SwiperSlide>
         <div className="bg-[#F1F1F1] p-4 text-center">
           <img src={star} className="mx-auto mb-2" />
-          <p>უფასო მასტერკლასები</p>
+          <p>ტექნოლოგიური ლექციები</p>
         </div>
+      </SwiperSlide>
+      <SwiperSlide>
         <div className="bg-[#F1F1F1] p-4 text-center">
           <img src={star} className="mx-auto mb-2" />
-          <p>უფასო მასტერკლასები</p>
+          <p>პროექტებზე მუშაობა</p>
         </div>
-      </Carousel>
-    </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
